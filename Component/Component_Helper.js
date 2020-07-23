d14bb44543f65fc3fe946b79ec7199d4382da09d
JMMlw <36853795+JMMlw@users.noreply.github.com>
2020-07-23 10:40:28 +0200

    Update Component_Helper.js

--- Component/Component_Helper.js
+++ Component/Component_Helper.js
@@ -1,6 +1,16 @@
 ({
     doInit : function(component, event, helper) {
         // DO SOME
+        
+        this.callServer(component,
+            "c.isCadreR2Fige",
+            function(response){
+                console.log('response : ' + response);
+            },
+            {
+                opportunityId : oppId
+            }
+        )
     }
     
 })
